1f91fd75825848c54b46ea3467208fbe4bb655c9
Andrei Dorobantu <elfandreis@gmail.com>
2022-10-13 12:09:36 +0300

    Added stitches and radix ui libraries

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,29 @@
 // src/pages/_app.tsx
-import "../styles/globals.css";
-import { SessionProvider } from "next-auth/react";
-import type { Session } from "next-auth";
-import type { AppType } from "next/app";
-import { trpc } from "../utils/trpc";
+import { SessionProvider } from 'next-auth/react';
+import type { Session } from 'next-auth';
+import type { AppType } from 'next/app';
+import { trpc } from '../utils/trpc';
+import { theme } from '../styles/stitches.config';
+import { ThemeProvider } from 'next-themes';
+import { globalStyles } from '../styles';
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
-  pageProps: { session, ...pageProps },
+  pageProps: { session, ...pageProps }
 }) => {
+  globalStyles();
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="light"
+        value={{
+          dark: theme.dark.className,
+          light: theme.light.className
+        }}
+      >
+        <Component {...pageProps} />
+      </ThemeProvider>
     </SessionProvider>
   );
 };
